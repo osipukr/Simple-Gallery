f8570a69aa8032ae51b97f2951d1be1b96c153ee
FredStone <fredstone624@gmail.com>
2018-12-23 02:41:43 +0300

    finish

--- Simply-Gallery.DAL/Entities/ApplicationRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-
-namespace Simply_Gallery.DAL.Entities
-{
-    public class ApplicationRole : IdentityRole
-    {
-        public string Description { get; set; }
-    }
-}
--- Simply-Gallery.DAL/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-
-namespace Simply_Gallery.DAL.Entities
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public virtual ClientProfile ClientProfile { get; set; }
-    }
-}
