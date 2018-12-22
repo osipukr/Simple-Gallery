a1a50065e685be5483cd33dce3fe1abf4154497e
FredStone <fredstone624@gmail.com>
2018-12-23 01:33:33 +0300

    add UnitOfWork

--- Simply-Gallery.DAL/Entities/ClientProfile.cs
+++ Simply-Gallery.DAL/Entities/ClientProfile.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Simply_Gallery.DAL.Entities
 {
-    //  Разделение данных о пользователя на два класса позволит 
+    // Разделение данных о пользователя на два класса позволит 
     // независимо друг от друга рассматривать аутентификационные 
     // данные и вспомогательные данные, то есть профиль пользователя,
     // которые не играют никакой роли при аутентификации.
@@ -15,7 +15,12 @@ namespace Simply_Gallery.DAL.Entities
         public string Id { get; set; }
 
         public string Name { get; set; }
-        public string Address { get; set; }
+
+        public string Role { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public string ImageMimeType { get; set; }
 
         public virtual ApplicationUser ApplicationUser { get; set; }
     }
